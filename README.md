a4f52bd91dd1cf770bc2d738c9c7f668063e7804
wk <somnuk.wk@gmail.com>
2022-06-07 20:48:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DocFX
+
+```
+brew install docfx
+docfx init
+```
+
+# Build
+
+```
+docfx build docfx_project/docfx.json -o docs
+```
---
+++ docfx_project/src/MyClient/Hello.cs
@@ -0,0 +1,17 @@
+namespace MyClient
+{
+    /// <summary>
+    ///  This is H
+    /// </summary>
+    public class H
+    {
+        /// <summary>
+        ///  A hello
+        /// </summary>
+        public void A()
+        {
+
+        }
+
+    }
+}
